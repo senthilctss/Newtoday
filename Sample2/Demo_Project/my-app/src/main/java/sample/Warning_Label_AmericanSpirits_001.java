8af5ba3730903afbbfb837ad9af2a02e3a98bb0d
Senthil <skumar.scm@gmail.com>
2020-08-17 15:54:53 +0530

    added

---
+++ Sample2/Demo_Project/my-app/src/main/java/sample/Warning_Label_AmericanSpirits_001.java
@@ -0,0 +1,15 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+
+public class Warning_Label_AmericanSpirits_001 {
+
+	@Test
+	public void testjobs(){
+
+		System.out.println("Welcome TestNG test");
+
+	}
+
+}
